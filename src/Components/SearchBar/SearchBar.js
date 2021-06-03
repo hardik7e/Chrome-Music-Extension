ff3ce383afb0630d92b0457fe01f7bdd0c1cb204
unknown <kbimal53@gmail.com>
2021-06-03 18:21:01 +0530

    added

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -4,7 +4,7 @@ import {Grid, TextField} from '@material-ui/core'
 function SearchBar() {
     return (
         <Grid container>
-            <TextField placeholder="Search Song/Artist Name" fullWidth margin="normal" variant="outlined"></TextField>
+            <TextField placeholder="Search Song by Artist Name" fullWidth margin="normal" variant="outlined"></TextField>
         </Grid>
     )
 }
