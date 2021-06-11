993fc31ba82b7da94e0545dd8821845a4b544a4f
unknown <hardik7e@gmail.com>
2021-06-11 17:24:52 +0530

    UI changes to Music Plaer

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import SearchBar from './Components/SearchBar/SearchBar'
 import { Grid } from '@material-ui/core';
 import Playlist from './Components/Playlist/Playlist'
 import AudioPlayer from './Components/AudioPlayer/AudioPlayer'
-import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
 
 
 function App() {
   return (
-    <Grid container direction="column" style={{padding:'10px'}} justify="space-between">
-        <SearchBar/>
-        <Playlist/>
-        <AudioPlayer/>
-    </Grid>
+    <div style={{minWidth:'500px',minHeight:'500px',flexGrow:'1'}}>
+      <Grid container direction="column" style={{padding:'10px',height:'100vh'}} justify="space-between" >
+          <Grid item style={{position:'relative', left:'0px', top:'0px'}}><SearchBar /></Grid>
+          <Grid item style={{position:'relative', left:'0px', top:'3px'}}><Playlist/></Grid>
+          <Grid item style={{position:'relative', left:'0px', bottom:'0px'}}><AudioPlayer/></Grid>
+      </Grid>
+    </div>
   );
 }
 
